a0f26c8a989efee6adbc74d8ec312240bff86cfd
Rabs <rabbolinif@gmail.com>
2022-03-25 12:13:00 +0100

    React base skeleton with routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import {Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
 const App = () => {
-  return <div>Hello there</div>
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='new-post' element={<CreatePost />} />
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
